2ff8428511194179a34cb7cf9195ed7e2cde82d0
CGB <rn1646ic@gmail.com>
2021-05-13 01:30:36 -0500

    Last commit before changing Tetrimo storage paradigm

--- tetris/main.c
+++ tetris/main.c
@@ -13,16 +13,30 @@
 
 // tetrimos defined in main.h
 
+const int ROW_WIDTH = DEFAULT_WIDTH + COMBINED_WALL_WIDTH;
+
 const char TETRIMOS[7][8] = 
 {
-    "xxxx    ", // I
-    "x   xxx ", // J
-    "  x xxx ", // L
-    "xx  xx  ", // O 
-    " xx xx  ", // S
-    " x  xxx ", // T
-    "xx   xx "  // Z
+    "xcxx----", // I
+    "x---cxx-", // J
+    "--x-xxc-", // L
+    "cc--cc--", // O 
+    "-xc cx--", // S
+    "-x--xcx-", // T
+    "xc---cx-"  // Z
 };
+
+// Tetrimos, when rotated, are off from their center. This array stores the offset for each Tetrimo to return to its center
+// const char TETRIMO_OFFSET[7][3] = 
+// {
+//     {1 - (2 * ROW_WIDTH), -1 - ROW_WIDTH, -ROW_WIDTH}, // I
+//     {-1 - (2 * ROW_WIDTH), -2 + ROW_WIDTH, ROW_WIDTH}, // J
+//     {1, 2, 3}, // L
+//     {-2 * ROW_WIDTH, -2, 3}, // O
+//     {1, 2, 3}, // S
+//     {1, 1, -ROW_WIDTH}, // T
+//     {1, 2, 3}  // Z
+// };
 
 struct winsize window;
 
@@ -35,6 +49,7 @@ struct winsize window;
 *       EX: Terminal Size, exit code, etc
 *
 */
+
 
 void onScreenResize(int sig){
     ioctl(fileno(stdout), TIOCGWINSZ, &window);
@@ -85,6 +100,7 @@ void drawTetrimo(char frameData[], int tetrimoFrameIndex, int tetrimoTypeIndex, 
             // for (int i = 4; i >= 0; i--){
             //     frameData[currentTetrimoFrameIndex + rowSize + i] = TETRIMOS[tetrimoTypeIndex][i];
             // }
+            //tetrimoFrameIndex = tetrimoFrameIndex + 1; // test offset
             for (int r = 0; r < 2; r++){
                 for (int c = 0; c < 4; c++){
                     frameData[tetrimoFrameIndex + (rowSize * r) + c] = TETRIMOS[tetrimoTypeIndex][7 - (c + (r * 4))];
@@ -98,6 +114,7 @@ void drawTetrimo(char frameData[], int tetrimoFrameIndex, int tetrimoTypeIndex, 
             // for (int i = 4; i >= 0; i--){
             //     frameData[currentTetrimoFrameIndex + (rowSize * i + 1)] = TETRIMOS[tetrimoTypeIndex][i];
             // }
+            // tetrimoFrameIndex = tetrimoFrameIndex - rowSize;
             for (int r = 0; r < 4; r++){
                 for (int c = 0; c < 2; c++){
                     frameData[tetrimoFrameIndex + (rowSize * r) + c] = TETRIMOS[tetrimoTypeIndex][7 - ((c * 4) + 3 - r)];
@@ -113,18 +130,19 @@ void drawTetrimo(char frameData[], int tetrimoFrameIndex, int tetrimoTypeIndex, 
 
 // Called whenever a Tetrimo is spawned. For a spawned Tetrimo, updateTetrimo() is called
 void spawnTetrimo(char frameData[], int tetrimoIndex){
-    int spawnIndex = (DEFAULT_WIDTH / 2);
+    // FIXME: Cleaner way to find spawn index? Define frame top buffer instead of using explicit value?
+    int spawnIndex = (DEFAULT_WIDTH / 2) + (4 * (DEFAULT_WIDTH + COMBINED_WALL_WIDTH));
     if (DEFAULT_WIDTH > spawnIndex + 4){
         printf("ERROR: Default width too small! Currently %d", DEFAULT_WIDTH);
         exit(EXIT_FAILURE); 
     }
-        
+    
     drawTetrimo(frameData, spawnIndex, tetrimoIndex, 0);
 }
 
 // returns 1 when tetrimo is 'set', otherwise returns 0
 int updateTetrimo(char frameData[], int currentTetrimoFrameIndex, int tetrimoTypeIndex, int rotation){
-    
+    return -1;
 }
 
 void printPadding(int padding){
@@ -133,17 +151,21 @@ void printPadding(int padding){
     }
 }
 
-void drawFrame(char frameData[], int frameDataSize, int frameWidth){
+void drawFrame(char frameData[], int frameDataSize){
     int terminalColSize = window.ws_col;
-    int frameHeight = frameDataSize / frameWidth;
+    int frameWidth = DEFAULT_WIDTH + COMBINED_WALL_WIDTH;
+    // int frameHeight = (frameDataSize / frameWidth) - 4;
 
     // The Tetris grid is DEFAULT_WIDTH wide, DEFAULT_HEIGHT tall. To draw it in the center,
     // we have to calulate the center of the terminal columns, then subtract DEFAULT_WIDTH / 2 from that
     // to get the correct padding on the left side of the Tetris grid, and since its symmetrical, the right aswell.
 
-    int paddingW = (terminalColSize / 2) - ((DEFAULT_WIDTH + COMBINED_WALL_WIDTH) / 2);
+    int paddingW = (terminalColSize / 2) - ((frameWidth) / 2);
 
-    for (int h = 0; h < frameDataSize; h += frameWidth) {
+    // The grid is drawn starting at frameWidth * 4 because the frame extends 4 
+    // places above the screen to allow for pieces to spawn above the board and be offset correctly.
+
+    for (int h = frameWidth * 4; h < frameDataSize; h += frameWidth) {
 
         // Prints out the padding on the left side
         printPadding(paddingW);
@@ -175,17 +197,26 @@ void fillRowWithSecondArg(char frameData[], char c, int rowWidth, int currentHei
 }
 
 // Called at the beginning of a game to get a char array with walls characters filled and empty spaces 
-void buildCleanFrame(char frameData[],  int frameDataSize, int frameWidth){
-    int rowWidth = frameWidth - COMBINED_WALL_WIDTH;
+void buildCleanFrame(char frameData[],  int frameDataSize){
+    int frameWidth = DEFAULT_WIDTH + COMBINED_WALL_WIDTH;
 
     for (int h = 0; h < frameDataSize - frameWidth; h += frameWidth){
-        fillRowWithSecondArg(frameData, ' ', rowWidth, h);
+        fillRowWithSecondArg(frameData, ' ', DEFAULT_WIDTH, h);
     }
-   fillRowWithSecondArg(frameData, '_', rowWidth, frameDataSize - frameWidth);
+
+   fillRowWithSecondArg(frameData, '_', DEFAULT_WIDTH, frameDataSize - frameWidth);
+}
+
+// runs a demo cycle through tetrimo pieces and their rotations, must be called on by Update to run
+void demo(char frameData[], int frameDataSize, int frameNumber){
+    int rotation = (frameNumber - 1) % 4;
+    int tetrimoIndex = ((frameNumber - 1) / 4) % 7;
+    buildCleanFrame(frameData, frameDataSize);
+    drawTetrimo(frameData, 53, tetrimoIndex, rotation);
 }
 
 // Called once per 'frame' while running. Returns 1 on success, 0 on error, and a negative value on exit
-int update(char frameData[], int frameDataSize, int frameWidth, int frameNumber){
+int update(char frameData[], int frameDataSize, int frameNumber){
     // TODO: Implement input that does not interrupt system
     // char input;
 
@@ -209,15 +240,18 @@ int update(char frameData[], int frameDataSize, int frameWidth, int frameNumber)
     //         break;
     // }
 
-    int rotation = (frameNumber - 1) % 4;
-    buildCleanFrame(frameData, frameDataSize, frameWidth);
-    drawTetrimo(frameData, 5, 2, rotation);
-   
+    demo(frameData, frameDataSize, frameNumber);
+
+    // int rotation = (frameNumber - 1) % 4;
+    // buildCleanFrame(frameData, frameDataSize, frameWidth);
+    // drawTetrimo(frameData, 5, 5, rotation);
 
     printf("----Frame Number %d----\n", frameNumber);
-    drawFrame(frameData, frameDataSize, frameWidth);
+    drawFrame(frameData, frameDataSize);
     return 1;
 }
+
+// run function here (while loop from main)
 
 int main(int argc, char** argv){
     // check arguments, if there are 2 arguments, or 1 argument, then process input. if only 1 arg, set the default height to twenty
@@ -225,9 +259,9 @@ int main(int argc, char** argv){
     // define char array of size DEFAULT_WIDTH + 2 for drawing the walls, otherwise 
 
     int frameWidth = DEFAULT_WIDTH + COMBINED_WALL_WIDTH;
-    int frameHeight = DEFAULT_HEIGHT + 1;
+    int frameHeight = DEFAULT_HEIGHT;
 
-    int frameDataSize = frameWidth * frameHeight;
+    int frameDataSize = frameWidth * (frameHeight + 4); // we add four to the height to have extra space for spawned tetrimos
     char frameData[frameDataSize];
 
     // terminal = initscr(); // CURSES.h
@@ -237,7 +271,7 @@ int main(int argc, char** argv){
     ioctl(fileno(stdout), TIOCGWINSZ, &window); // column width and height of terminal set
 
     printf("Welcome to Tetris!\n");
-    buildCleanFrame(frameData, frameDataSize, frameWidth);
+    buildCleanFrame(frameData, frameDataSize);
     // for (int i = 0; i < frameDataSize; i++){
     //     frameData[i] = 'x';
     // }
@@ -247,7 +281,7 @@ int main(int argc, char** argv){
     int running = 1;
 
     while (running) {
-        running = update(frameData, frameDataSize, frameWidth, frameNumber);
+        running = update(frameData, frameDataSize, frameNumber);
         frameNumber++;
         printf("\n");
         sleep(2);
---
+++ tetris/main.h
@@ -0,0 +1,54 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+// const char TETRIMOS[7][8] = 
+// {
+//     "xxxx    ", // I
+//     "x   xxx ", // J
+//     "  x xxx ", // L
+//     "xx  xx  ", // O 
+//     " xx xx  ", // S
+//     " x  xxx ", // T
+//     "xx   xx "  // Z
+// };
+
+// const char TETRIMOS[28][16] = 
+// {
+//     "xxxx            ", // Default I
+//     "x   x   x   x   ", // rotated once
+//     "            xxxx", // rotated twice
+//     "   x   x   x   x", // rotated three times, End of I
+
+//     "x   xxx         ", // Default J
+//     "x   x   x   x   ", // rotated once
+//     "xxxx            ", // rotated twice
+//     "xxxx            ", // rotated three times, End of I
+
+//     "xxxx            ", // Default I
+//     "x   x   x   x   ", // rotated once
+//     "xxxx            ", // rotated twice
+//     "xxxx            ", // rotated three times, End of I
+
+//     "xxxx            ", // Default I
+//     "x   x   x   x   ", // rotated once
+//     "xxxx            ", // rotated twice
+//     "xxxx            ", // rotated three times, End of I
+
+//     "xxxx            ", // Default I
+//     "x   x   x   x   ", // rotated once
+//     "xxxx            ", // rotated twice
+//     "xxxx            ", // rotated three times, End of I
+
+//     "xxxx            ", // Default I
+//     "x   x   x   x   ", // rotated once
+//     "xxxx            ", // rotated twice
+//     "xxxx            ", // rotated three times, End of I
+
+//     "xxxx            ", // Default I
+//     "x   x   x   x   ", // rotated once
+//     "xxxx            ", // rotated twice
+//     "xxxx            "  // rotated three times, End of I
+// };
+
+
+#endif
